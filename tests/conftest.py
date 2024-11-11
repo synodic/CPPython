ac184d836e4669b683554ae59be82c019604fc6e
Asher Norland <asher.norland@gmail.com>
2024-11-11 16:38:01 -0500

    Ya

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Data variations for testing"""
 
 # from pathlib import Path
+from pathlib import Path
 from typing import cast
 
 import pytest
@@ -40,3 +41,36 @@ def fixture_cmake_data(request: pytest.FixtureRequest) -> CMakeConfiguration:
     """
 
     return cast(CMakeConfiguration, request.param)
+
+
+def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
+    """Called for each test function
+
+    Args:
+        metafunc: Pytest hook data
+    """
+
+    for fixture in metafunc.fixturenames:
+        match fixture.split("_", 1):
+            case ["internal", "plugin_data_path"]:
+                # There should only ever be one fixture named 'internal_plugin_data_path' for value caching
+                data_path = metafunc.config.rootpath / "tests" / "data"
+
+                test_paths: list[Path | None] = []
+
+                for path in data_path.glob("*"):
+                    if path.is_dir():
+                        test_paths.append(path)
+
+                if not test_paths:
+                    test_paths = [None]
+                metafunc.parametrize(fixture, test_paths, scope="session")
+
+            case ["internal", "data_path"]:
+                # There should only ever be one fixture named 'internal_data_path' for value caching
+                data_path = Path(__file__).parent / "data"
+                metafunc.parametrize(fixture, list(data_path.glob("*")), scope="session")
+
+            case ["build", directory]:
+                data_path = metafunc.config.rootpath / "tests" / "build" / directory
+                metafunc.parametrize(fixture, [data_path], scope="session")
