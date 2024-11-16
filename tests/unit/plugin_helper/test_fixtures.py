24d681ff7510da567e37dabcf8d247eaa1d3bb18
Asher Norland <asher.norland@gmail.com>
2024-11-16 00:53:32 -0500

    Fix Tests

--- tests/unit/plugin_helper/test_fixtures.py
+++ tests/unit/plugin_helper/test_fixtures.py
@@ -6,17 +6,6 @@ from pathlib import Path
 class TestFixtures:
     """Tests for fixtures"""
 
-    def test_pyproject_undefined(self, data_path: Path) -> None:
-        """Verifies that the directory data provided by pytest_cppython contains a pyproject.toml file
-
-        Args:
-            data_path: The project's tests/data directory
-        """
-
-        paths = list(data_path.rglob("pyproject.toml"))
-
-        assert len(paths) == 1
-
     def test_data_directory(self, plugin_data_path: Path | None) -> None:
         """Verifies that the directory data provided by pytest_cppython contains a pyproject.toml file
 
@@ -25,10 +14,7 @@ class TestFixtures:
         """
 
         assert plugin_data_path is not None
-
-        requirement = plugin_data_path / "requirement.txt"
-
-        assert requirement.exists()
+        assert plugin_data_path.exists()
 
     def test_build_directory(self, build_test_build: Path) -> None:
         """Verifies that the build data provided is the expected path
