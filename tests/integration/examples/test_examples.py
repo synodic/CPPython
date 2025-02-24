221e6cead7f5af1a032b7fc322af2b10e04c3fc6
Asher Norland <asher.norland@gmail.com>
2025-02-24 00:33:21 -0500

    Revert "Ye"
    This reverts commit 2b2dff2bd7efe90ee4b7cf171d7084a0b298251f.

--- tests/integration/examples/test_examples.py
+++ tests/integration/examples/test_examples.py
@@ -3,58 +3,16 @@
 All examples can be run with the CPPython entry-point, and we use the examples as the test data for the CLI.
 """
 
-import shutil
 from pathlib import Path
 
-from typer.testing import CliRunner
-
-from cppython.console.entry import app
-
-runner = CliRunner()
 pytest_plugins = ['tests.fixtures.example']
 
 
 class TestExamples:
-    """Verifies the examples are accessible"""
+    """Tests to apply to all examples"""
 
     @staticmethod
     def test_example_directory(example_directory: Path) -> None:
         """Verify that the fixture is returning the right data"""
         assert example_directory.is_dir()
         assert (example_directory / 'pyproject.toml').is_file()
-
-    @staticmethod
-    def test_info(example_directory: Path) -> None:
-        """Verifies that the info command functions with CPPython hooks"""
-        with runner.isolated_filesystem() as temp_directory:
-            shutil.copytree(example_directory, temp_directory, dirs_exist_ok=True)
-
-            result = runner.invoke(app, ['info'])
-            assert result.exit_code == 0
-
-    @staticmethod
-    def test_list(example_directory: Path) -> None:
-        """Verifies that the list command functions with CPPython hooks"""
-        with runner.isolated_filesystem() as temp_directory:
-            shutil.copytree(example_directory, temp_directory, dirs_exist_ok=True)
-
-            result = runner.invoke(app, ['list'])
-            assert result.exit_code == 0
-
-    @staticmethod
-    def test_update(example_directory: Path) -> None:
-        """Verifies that the update command functions with CPPython hooks"""
-        with runner.isolated_filesystem() as temp_directory:
-            shutil.copytree(example_directory, temp_directory, dirs_exist_ok=True)
-
-            result = runner.invoke(app, ['update'])
-            assert result.exit_code == 0
-
-    @staticmethod
-    def test_install(example_directory: Path) -> None:
-        """Verifies that the install command functions with CPPython hooks"""
-        with runner.isolated_filesystem() as temp_directory:
-            shutil.copytree(example_directory, temp_directory, dirs_exist_ok=True)
-
-            result = runner.invoke(app, ['install'])
-            assert result.exit_code == 0
