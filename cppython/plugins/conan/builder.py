d936c2df1359e033cf2293b875f75d75c397b2c3
Asher Norland <asher.norland@gmail.com>
2025-04-17 15:51:35 -0400

    Template: Remove Toolchain Usage

--- cppython/plugins/conan/builder.py
+++ cppython/plugins/conan/builder.py
@@ -137,10 +137,6 @@ class Builder:
             def layout(self):
                 cmake_layout(self)
 
-            def generate(self):
-                tc = CMakeToolchain(self)
-                tc.generate()
-
             def build(self):
                 cmake = CMake(self)
                 cmake.configure()
