584592a3765fe43d561d9e7d186548335018f457
Asher Norland <asher.norland@gmail.com>
2025-07-18 15:01:04 -0400

    Build Requires Example

--- examples/conan_cmake/build_requires/main.cpp
+++ examples/conan_cmake/build_requires/main.cpp
@@ -1,7 +1,25 @@
-#include "fmt/color.h"
+#include <iostream>
+#include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
+#include "absl/base/macros.h"
 
 int main()
 {
-    fmt::print(fg(fmt::terminal_color::cyan), "Hello fmt {}!\n", FMT_VERSION);
+    // Demonstrate abseil string formatting
+    absl::string_view greeting = "Hello";
+    std::string name = "Abseil";
+
+    // Use absl::StrFormat instead of printf-style formatting
+    std::string formatted = absl::StrFormat("%s %s! Version: %s",
+                                            greeting, name,
+                                            ABSL_PACKAGE_VERSION);
+
+    std::cout << formatted << std::endl;
+
+    // Demonstrate some abseil string operations
+    std::string message = absl::StrFormat("This example shows tool_requires with CMake %s",
+                                          "working correctly!");
+    std::cout << message << std::endl;
+
     return 0;
 }
