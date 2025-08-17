4214af145cb09f9b7d5b387063c83445123e635e
Asher Norland <asher.norland@gmail.com>
2025-08-17 18:04:48 -0400

    Update builder.py

--- cppython/plugins/conan/builder.py
+++ cppython/plugins/conan/builder.py
@@ -158,7 +158,6 @@ class Builder:
 
             def export_sources(self):
                 copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
-                copy(self, "include/*", src=self.recipe_folder, dst=self.export_sources_folder)
                 copy(self, "src/*", src=self.recipe_folder, dst=self.export_sources_folder)
                 copy(self, "cmake/*", src=self.recipe_folder, dst=self.export_sources_folder)
             """
